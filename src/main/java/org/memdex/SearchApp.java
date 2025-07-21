89faef335b107d728a37a98e8e2de4001311ea19
NikhilNawani <nikhilnikhil242004@gmail.com>
2025-07-21 06:14:46 +0530

    SeachApp

--- src/main/java/org/memdex/SearchApp.java
+++ src/main/java/org/memdex/SearchApp.java
@@ -5,5 +5,32 @@ import java.util.List;
 import java.io.IOException;
 
 public class SearchApp {
+    public static void main(String []args) {
+        System.out.println("Starting In-Memory Search Engine...");
+        Indexer indexer = new Indexer();
 
+        try {
+            // 1 Indexing Phase
+            System.out.println("Indexing document..");
+            indexer.indexFile("test-data/file1.txt");
+            indexer.indexFile("test-data/file2.txt");
+            System.out.println("Indexing complete.");
+
+            // Searching phase
+            String searchTerm = "brown";
+            System.out.println("\nSearching for term: '"+ searchTerm+"'");
+
+            List<String> result = indexer.search(searchTerm);
+
+            if(result.isEmpty()) {
+                System.out.println("Term not found");
+            } else {
+                System.out.println("Found in the following ");
+                result.forEach(System.out::println);
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred: "+ e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
